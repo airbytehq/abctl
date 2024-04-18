49f7ae872bce2adf764f7fd491ac8f4b51fcc19c
Cole Snodgrass <cole@airbyte.io>
2024-04-18 15:35:54 -0700

    feat: improve version output (#10)

--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -21,27 +21,37 @@ func TestCmd_Output(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if d := cmp.Diff(fmt.Sprintf("version: %s\n", build.Version), b.String()); d != "" {
+	exp := fmt.Sprintf("version: %s\nrevision: %s\ntime: %s\nmodified: %t\n", build.Version, build.Revision, build.ModificationTime, build.Modified)
+
+	if d := cmp.Diff(exp, b.String()); d != "" {
 		t.Error("cmd mismatch (-want +got):\n", d)
 	}
 }
 
-func TestCmd_OutputVersionOverride(t *testing.T) {
+func TestCmd_OutputOverride(t *testing.T) {
 	origVersion := build.Version
 	build.Version = "v12.15.82"
+	build.Revision = "revision"
+	build.Modified = true
+	build.ModificationTime = "20240401T00:00:00Z"
 	b := bytes.NewBufferString("")
 	pterm.SetDefaultOutput(b)
 
 	t.Cleanup(func() {
 		pterm.SetDefaultOutput(os.Stdout)
 		build.Version = origVersion
+		build.Revision = ""
+		build.Modified = false
+		build.ModificationTime = ""
 	})
+
+	exp := fmt.Sprintf("version: %s\nrevision: %s\ntime: %s\nmodified: %t\n", build.Version, build.Revision, build.ModificationTime, build.Modified)
 
 	if err := Cmd.Execute(); err != nil {
 		t.Fatal(err)
 	}
 
-	if d := cmp.Diff(fmt.Sprintf("version: %s\n", build.Version), b.String()); d != "" {
+	if d := cmp.Diff(exp, b.String()); d != "" {
 		t.Error("cmd mismatch (-want +got):\n", d)
 	}
 }
