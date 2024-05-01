310a311ad581fa6a4246851729730cb646b73069
Cole Snodgrass <cole@airbyte.io>
2024-05-01 15:33:48 -0700

    remove commented out code

--- internal/cmd/local/prerun.go
+++ internal/cmd/local/prerun.go
@@ -5,40 +5,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//// persistentPreRunLocal is extracted from the cobra.Command initialization for reading purposes only
-//func persistentPreRunLocal(cmd *cobra.Command, _ []string) error {
-//	// telemetry client configuration
-//	{
-//		// ignore the error as it will default to false if an error returns
-//		dnt, _ := cmd.Flags().GetBool("dnt")
-//		var telOpts []telemetry.GetOption
-//		if dnt {
-//			telOpts = append(telOpts, telemetry.WithDnt())
-//		}
-//
-//		telClient = telemetry.Get(telOpts...)
-//	}
-//	// provider configuration
-//	{
-//		var err error
-//		provider, err = k8s.ProviderFromString(flagProvider)
-//		if err != nil {
-//			return err
-//		}
-//
-//		printK8sProvider(provider)
-//	}
-//
-//	return nil
-//}
-
-//func printK8sProvider(p k8s.Provider) {
-//	userHome, _ := os.UserHomeDir()
-//	configPath := filepath.Join(userHome, p.Kubeconfig)
-//	pterm.Info.Printfln("using kubernetes provider:\n  provider name: %s\n  kubeconfig: %s\n  context: %s",
-//		p.Name, configPath, p.Context)
-//}
-
 // preRunInstall is extracted from the cobra.Command initialization for reading purposes only
 func preRunInstall(cmd *cobra.Command, _ []string) error {
 	if err := dockerInstalled(cmd.Context(), telClient); err != nil {
