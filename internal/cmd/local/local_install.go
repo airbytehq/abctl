a1712c752eec8e45585d5c2bee3af14e3d553a51
Cole Snodgrass <cole@airbyte.io>
2024-05-13 14:17:03 -0700

    update port flag

--- internal/cmd/local/local_install.go
+++ internal/cmd/local/local_install.go
@@ -131,7 +131,7 @@ func NewCmdInstall(provider k8s.Provider) *cobra.Command {
 
 	cmd.Flags().StringVarP(&flagUsername, "username", "u", "airbyte", "basic auth username, can also be specified via "+envBasicAuthUser)
 	cmd.Flags().StringVarP(&flagPassword, "password", "p", "password", "basic auth password, can also be specified via "+envBasicAuthPass)
-	cmd.Flags().IntVarP(&flagPort, "port", "", local.Port, "ingress http port")
+	cmd.Flags().IntVar(&flagPort, "port", local.Port, "ingress http port")
 
 	cmd.Flags().StringVar(&flagHelmChartVersion, "helm-chart-version", "latest", "specify the specific Airbyte helm-chart-version to install")
 
