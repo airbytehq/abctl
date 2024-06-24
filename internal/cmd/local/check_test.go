a9e5139ad5fa19a241bfad583ed1855e6ed88b5d
Cole Snodgrass <cole@airbyte.io>
2024-06-24 09:34:57 -0700

    update docker mocks

--- internal/cmd/local/check_test.go
+++ internal/cmd/local/check_test.go
@@ -7,8 +7,13 @@ import (
 	"github.com/airbytehq/abctl/internal/cmd/local/localerr"
 	"github.com/airbytehq/abctl/internal/telemetry"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/api/types/volume"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/uuid"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"io"
 	"net"
 	"strconv"
 	"strings"
@@ -115,16 +120,63 @@ func port(s string) int {
 var _ docker.Client = (*mockDockerClient)(nil)
 
 type mockDockerClient struct {
-	serverVersion    func(ctx context.Context) (types.Version, error)
-	containerInspect func(ctx context.Context, containerID string) (types.ContainerJSON, error)
+	containerCreate   func(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, platform *ocispec.Platform, containerName string) (container.CreateResponse, error)
+	containerInspect  func(ctx context.Context, containerID string) (types.ContainerJSON, error)
+	containerRemove   func(ctx context.Context, container string, options container.RemoveOptions) error
+	containerStart    func(ctx context.Context, container string, options container.StartOptions) error
+	containerStop     func(ctx context.Context, container string, options container.StopOptions) error
+	copyFromContainer func(ctx context.Context, container, srcPath string) (io.ReadCloser, types.ContainerPathStat, error)
+
+	containerExecCreate  func(ctx context.Context, container string, config types.ExecConfig) (types.IDResponse, error)
+	containerExecInspect func(ctx context.Context, execID string) (types.ContainerExecInspect, error)
+	containerExecStart   func(ctx context.Context, execID string, config types.ExecStartCheck) error
+
+	serverVersion func(ctx context.Context) (types.Version, error)
+	volumeInspect func(ctx context.Context, volumeID string) (volume.Volume, error)
+}
+
+func (m mockDockerClient) ContainerCreate(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, platform *ocispec.Platform, containerName string) (container.CreateResponse, error) {
+	return m.containerCreate(ctx, config, hostConfig, networkingConfig, platform, containerName)
 }
 
 func (m mockDockerClient) ContainerInspect(ctx context.Context, containerID string) (types.ContainerJSON, error) {
 	return m.containerInspect(ctx, containerID)
 }
 
+func (m mockDockerClient) ContainerRemove(ctx context.Context, container string, options container.RemoveOptions) error {
+	return m.containerRemove(ctx, container, options)
+}
+
+func (m mockDockerClient) ContainerStart(ctx context.Context, container string, options container.StartOptions) error {
+	return m.containerStart(ctx, container, options)
+}
+
+func (m mockDockerClient) ContainerStop(ctx context.Context, container string, options container.StopOptions) error {
+	return m.containerStop(ctx, container, options)
+}
+
+func (m mockDockerClient) CopyFromContainer(ctx context.Context, container, srcPath string) (io.ReadCloser, types.ContainerPathStat, error) {
+	return m.copyFromContainer(ctx, container, srcPath)
+}
+
+func (m mockDockerClient) ContainerExecCreate(ctx context.Context, container string, config types.ExecConfig) (types.IDResponse, error) {
+	return m.containerExecCreate(ctx, container, config)
+}
+
+func (m mockDockerClient) ContainerExecInspect(ctx context.Context, execID string) (types.ContainerExecInspect, error) {
+	return m.containerExecInspect(ctx, execID)
+}
+
+func (m mockDockerClient) ContainerExecStart(ctx context.Context, execID string, config types.ExecStartCheck) error {
+	return m.containerExecStart(ctx, execID, config)
+}
+
 func (m mockDockerClient) ServerVersion(ctx context.Context) (types.Version, error) {
 	return m.serverVersion(ctx)
+}
+
+func (m mockDockerClient) VolumeInspect(ctx context.Context, volumeID string) (volume.Volume, error) {
+	return m.volumeInspect(ctx, volumeID)
 }
 
 var _ telemetry.Client = (*mockTelemetryClient)(nil)
