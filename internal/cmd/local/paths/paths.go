7a02146d30b7d9f219969b67b410e5ab5b43a2d8
Cole Snodgrass <cole@airbyte.io>
2024-06-25 20:40:39 -0700

    add comments

--- internal/cmd/local/paths/paths.go
+++ internal/cmd/local/paths/paths.go
@@ -5,15 +5,18 @@ import (
 	"path/filepath"
 )
 
-// UserHome is the user's home directory
 var (
+	// UserHome is the user's home directory
 	UserHome = func() string {
 		h, _ := os.UserHomeDir()
 		return h
 	}()
+	// Airbyte is the full path to the ~/.airbyte directory
 	Airbyte = airbyte()
-	AbCtl   = abctl()
-	Data    = data()
+	// AbCtl is the full path to the ~/.airbyte/abctl directory
+	AbCtl = abctl()
+	// Data is the full path to the ~/.airbyte/abctl/data directory
+	Data = data()
 )
 
 func airbyte() string {
