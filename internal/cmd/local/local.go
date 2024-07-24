a52f0a6e58b265dd2afdcb68892c566fcfc54a16
Cole Snodgrass <cole@airbyte.io>
2024-07-24 14:20:30 -0700

    feat: attempt to fix invalid ~/.airbyte permissions (#61)

--- internal/cmd/local/local.go
+++ internal/cmd/local/local.go
@@ -1,10 +1,16 @@
 package local
 
 import (
+	"errors"
+	"fmt"
 	"github.com/airbytehq/abctl/internal/cmd/local/k8s"
+	"github.com/airbytehq/abctl/internal/cmd/local/localerr"
+	"github.com/airbytehq/abctl/internal/cmd/local/paths"
 	"github.com/airbytehq/abctl/internal/telemetry"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
+	"io/fs"
+	"os"
 )
 
 var telClient telemetry.Client
@@ -14,6 +20,10 @@ func NewCmdLocal(provider k8s.Provider) *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "local",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			if err := checkAirbyteDir(); err != nil {
+				return fmt.Errorf("%w: %w", localerr.ErrAirbyteDir, err)
+			}
+
 			// telemetry client configuration
 			{
 				var telOpts []telemetry.GetOption
@@ -39,5 +49,40 @@ func NewCmdLocal(provider k8s.Provider) *cobra.Command {
 }
 
 func printProviderDetails(p k8s.Provider) {
-	pterm.Info.Printfln("Using Kubernetes provider:\n  Provider: %s\n  Kubeconfig: %s\n  Context: %s", p.Name, p.Kubeconfig, p.Context)
+	pterm.Info.Println(fmt.Sprintf(
+		"Using Kubernetes provider:\n  Provider: %s\n  Kubeconfig: %s\n  Context: %s",
+		p.Name, p.Kubeconfig, p.Context,
+	))
+}
+
+// checkAirbyteDir verifies that, if the paths.Airbyte directory exists, that it has proper permissions.
+// If the directory does not have the proper permissions, this method will attempt to fix them.
+// A nil response either indicates that either:
+// - no paths.Airbyte directory exists
+// - the permissions are already correct
+// - this function was able to fix the incorrect permissions.
+func checkAirbyteDir() error {
+	fileInfo, err := os.Stat(paths.Airbyte)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			// nothing to do, directory will be created later on
+			return nil
+		}
+		return fmt.Errorf("unable to determine status of '%s': %w", paths.Airbyte, err)
+	}
+
+	if !fileInfo.IsDir() {
+		return errors.New(paths.Airbyte + " is not a directory")
+	}
+
+	if fileInfo.Mode().Perm() >= 0744 {
+		// directory has minimal permissions
+		return nil
+	}
+
+	if err := os.Chmod(paths.Airbyte, 0744); err != nil {
+		return fmt.Errorf("unable to change permissions of '%s': %w", paths.Airbyte, err)
+	}
+
+	return nil
 }
