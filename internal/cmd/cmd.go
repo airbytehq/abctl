9cd22c0fb5ed236d726081d4b2cfb7fb0fb30731
Cole Snodgrass <cole@airbyte.io>
2024-08-30 14:09:40 -0700

    combine errors with help messages

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -16,37 +16,6 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// Help messages to display for specific error situations.
-const (
-	// helpAirbyteDir is displayed if ErrAirbyteDir is ever returned
-	helpAirbyteDir = `The ~/.airbyte directory is inaccessible.
-You may need to remove this directory before trying your command again.`
-
-	// helpCluster is displayed if ErrClusterNotFound is ever returned
-	helpCluster = `No cluster was found. If this is unexpected,
-you may need to run the "local install" command again.`
-
-	// helpDocker is displayed if ErrDocker is ever returned
-	helpDocker = `An error occurred while communicating with the Docker daemon.
-Ensure that Docker is running and is accessible.  You may need to upgrade to a newer version of Docker.
-For additional help please visit https://docs.docker.com/get-docker/`
-
-	// helpKubernetes is displayed if ErrKubernetes is ever returned
-	helpKubernetes = `An error occurred while communicating with the Kubernetes cluster.
-If this error persists, you may need to run the uninstall command before attempting to run
-the install command again.`
-
-	// helpIngress is displayed if ErrIngress is ever returned
-	helpIngress = `An error occurred while configuring ingress.
-This could be in indication that the ingress port is already in use by a different application.
-The ingress port can be changed by passing the flag --port.`
-
-	// helpPort is displayed if ErrPort is ever returned
-	helpPort = `An error occurred while verifying if the request port is available.
-This could be in indication that the ingress port is already in use by a different application.
-The ingress port can be changed by passing the flag --port.`
-)
-
 func HandleErr(err error) {
 	if err == nil {
 		return
@@ -59,25 +28,10 @@ func HandleErr(err error) {
 		_ = kong.DefaultHelpPrinter(kong.HelpOptions{}, errParse.Context)
 	}
 
-	switch {
-	case errors.Is(err, localerr.ErrAirbyteDir):
+	var e *localerr.LocalError
+	if errors.As(err, &e) {
 		pterm.Println()
-		pterm.Info.Println(helpAirbyteDir)
-	case errors.Is(err, localerr.ErrClusterNotFound):
-		pterm.Println()
-		pterm.Info.Println(helpCluster)
-	case errors.Is(err, localerr.ErrDocker):
-		pterm.Println()
-		pterm.Info.Println(helpDocker)
-	case errors.Is(err, localerr.ErrKubernetes):
-		pterm.Println()
-		pterm.Info.Println(helpKubernetes)
-	case errors.Is(err, localerr.ErrIngress):
-		pterm.Println()
-		pterm.Info.Println(helpIngress)
-	case errors.Is(err, localerr.ErrPort):
-		pterm.Println()
-		pterm.Info.Printfln(helpPort)
+		pterm.Info.Println(e.Help())
 	}
 
 	os.Exit(1)
---
+++ internal/cmd/local/localerr/localerr_test.go
@@ -0,0 +1,30 @@
+package localerr
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestLocalError(t *testing.T) {
+	f := func() error {
+		return &LocalError{
+			help: "help message",
+			msg:  "error message",
+		}
+	}
+
+	err := f()
+	var e *LocalError
+	if !errors.As(err, &e) {
+		t.Fatal("error should be of type LocalError")
+	}
+
+	if d := cmp.Diff("help message", e.Help()); d != "" {
+		t.Errorf("help message diff:\n%s", d)
+	}
+	if d := cmp.Diff("error message", e.Error()); d != "" {
+		t.Errorf("error message diff:\n%s", d)
+	}
+}
