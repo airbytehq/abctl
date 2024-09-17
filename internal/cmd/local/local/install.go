d70ad704aed5a63e625d3de40601e4eec041bdb9
Cole Snodgrass <cole@airbyte.io>
2024-09-17 09:59:07 -0700

    fix: remove dynamic telemetry attr (#119)

--- internal/cmd/local/local/install.go
+++ internal/cmd/local/local/install.go
@@ -318,8 +318,6 @@ func (c *Command) diagnoseAirbyteChartFailure(ctx context.Context, chartErr erro
 
 		var errors []string
 		for _, pod := range podList.Items {
-			c.tel.Attr(fmt.Sprintf("pod-%s-phase", pod.Name), string(pod.Status.Phase))
-			
 			if pod.Status.Phase == corev1.PodFailed {
 				msg := "unknown"
 
