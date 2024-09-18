00ab45969299357c713b6e157e5ba3d58151d600
Cole Snodgrass <cole@airbyte.io>
2024-09-18 16:13:59 -0700

    fix: change what latest chart actually means (#128)

--- internal/cmd/local/local/locate.go
+++ internal/cmd/local/local/locate.go
@@ -1,13 +1,44 @@
 package local
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/pterm/pterm"
+	"golang.org/x/mod/semver"
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/getter"
 	"helm.sh/helm/v3/pkg/repo"
 )
+
+// chartRepo exists only for testing purposes.
+// This allows the DownloadIndexFile method to be mocked.
+type chartRepo interface {
+	DownloadIndexFile() (string, error)
+}
+
+var _ chartRepo = (*repo.ChartRepository)(nil)
+
+// newChartRepo exists only for testing purposes.
+// This allows a test implementation of the repo.NewChartRepository function to exist.
+type newChartRepo func(cfg *repo.Entry, getters getter.Providers) (chartRepo, error)
+
+// loadIndexFile exists only for testing purposes.
+// This allows a test implementation of the repo.LoadIndexFile function to exist.
+type loadIndexFile func(path string) (*repo.IndexFile, error)
+
+// defaultNewChartRepo is the default implementation of the newChartRepo function.
+// It simply wraps the repo.NewChartRepository function.
+// This variable should only be modified for testing purposes.
+var defaultNewChartRepo newChartRepo = func(cfg *repo.Entry, getters getter.Providers) (chartRepo, error) {
+	return repo.NewChartRepository(cfg, getters)
+}
+
+// defaultLoadIndexFile is the default implementation of the loadIndexFile function.
+// It simply wraps the repo.LoadIndexFile function.
+// This variable should only be modified for testing purposes.
+var defaultLoadIndexFile loadIndexFile = repo.LoadIndexFile
 
 func locateLatestAirbyteChart(chartName, chartVersion string) string {
 	pterm.Debug.Printf("getting helm chart %q with version %q\n", chartName, chartVersion)
@@ -32,7 +63,7 @@ func locateLatestAirbyteChart(chartName, chartVersion string) string {
 }
 
 func getLatestAirbyteChartUrlFromRepoIndex(repoName, repoUrl string) (string, error) {
-	chartRepo, err := repo.NewChartRepository(&repo.Entry{
+	chartRepository, err := defaultNewChartRepo(&repo.Entry{
 		Name: repoName,
 		URL:  repoUrl,
 	}, getter.All(cli.New()))
@@ -40,28 +71,46 @@ func getLatestAirbyteChartUrlFromRepoIndex(repoName, repoUrl string) (string, er
 		return "", fmt.Errorf("unable to access repo index: %w", err)
 	}
 
-	idxPath, err := chartRepo.DownloadIndexFile()
+	idxPath, err := chartRepository.DownloadIndexFile()
 	if err != nil {
 		return "", fmt.Errorf("unable to download index file: %w", err)
 	}
 
-	idx, err := repo.LoadIndexFile(idxPath)
+	idx, err := defaultLoadIndexFile(idxPath)
 	if err != nil {
 		return "", fmt.Errorf("unable to load index file (%s): %w", idxPath, err)
 	}
 
-	airbyteEntry, ok := idx.Entries["airbyte"]
+	entries, ok := idx.Entries["airbyte"]
 	if !ok {
 		return "", fmt.Errorf("no entry for airbyte in repo index")
 	}
 
-	if len(airbyteEntry) == 0 {
-		return "", fmt.Errorf("no chart version found")
+	if len(entries) == 0 {
+		return "", errors.New("no chart version found")
 	}
 
-	latest := airbyteEntry[0]
-	if len(latest.URLs) != 1 {
-		return "", fmt.Errorf("unexpected number of URLs")
+	var latest *repo.ChartVersion
+	for _, entry := range entries {
+		version := entry.Version
+		// the semver library requires a `v` prefix
+		if !strings.HasPrefix(version, "v") {
+			version = "v" + version
+		}
+
+		if semver.Prerelease(version) == "" {
+			latest = entry
+			break
+		}
 	}
+
+	if latest == nil {
+		return "", fmt.Errorf("no valid version of airbyte chart found in repo index")
+	}
+
+	if len(latest.URLs) != 1 {
+		return "", fmt.Errorf("unexpected number of URLs - %d", len(latest.URLs))
+	}
+
 	return airbyteRepoURL + "/" + latest.URLs[0], nil
 }
