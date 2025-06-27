88d16d52aa7e610c6ff84ada8cad6850a55372f4
Justin Pye <justin.pye@airbyte.io>
2025-06-26 18:36:51 -0700

    refactor: move Install, Status, Uninstall commands to internal/cmd/local

---
+++ internal/cmd/local/status.go
@@ -0,0 +1,37 @@
+package local
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/airbytehq/abctl/internal/common"
+	"github.com/pterm/pterm"
+	"go.opencensus.io/trace"
+)
+
+// Status handles the status of local Airbyte.
+func (c *Command) Status(ctx context.Context) error {
+	_, span := trace.StartSpan(ctx, "command.Status")
+	defer span.End()
+
+	charts := []string{common.AirbyteChartRelease, common.NginxChartRelease}
+	for _, name := range charts {
+		c.spinner.UpdateText(fmt.Sprintf("Verifying %s Helm Chart installation status", name))
+
+		rel, err := c.helm.GetRelease(name)
+		if err != nil {
+			pterm.Warning.Println("Unable to fetch airbyte release")
+			pterm.Debug.Printfln("unable to fetch airbyte release: %s", err)
+			continue
+		}
+
+		pterm.Info.Println(fmt.Sprintf(
+			"Found helm chart '%s'\n  Status: %s\n  Chart Version: %s\n  App Version: %s",
+			name, rel.Info.Status.String(), rel.Chart.Metadata.Version, rel.Chart.Metadata.AppVersion,
+		))
+	}
+
+	pterm.Info.Println(fmt.Sprintf("Airbyte should be accessible via http://localhost:%d", c.portHTTP))
+
+	return nil
+}
