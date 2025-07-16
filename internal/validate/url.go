8c4c351a0ab4134cfe99a0e00fbe3d10a869945c
Justin Pye <justin.pye@airbyte.io>
2025-07-15 17:09:39 -0700

    feat: validate.IsURL

---
+++ internal/validate/url.go
@@ -0,0 +1,11 @@
+package validate
+
+import "net/url"
+
+func IsURL(s string) bool {
+	u, err := url.Parse(s)
+	if err != nil {
+		return false
+	}
+	return (u.Scheme == "http" || u.Scheme == "https") && u.Host != ""
+}
