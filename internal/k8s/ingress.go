ffcee9a695c61f35e6cf1db0da45037366e441b3
Justin Pye <justin.pye@airbyte.io>
2025-08-06 12:26:06 -0700

    feat: route ingress traffic based on chart version
    Support backwards compatibility for webapp routing between v1 and v2+ charts.
    In v1 charts, the webapp is served from a dedicated webapp service, while in
    v2+ charts the webapp is served directly from the server service.
    
    - Add chart version parameter to Ingress function
    - Add version-specific routing logic
    - Add test coverage for version boundaries and routing behavior

--- internal/k8s/ingress.go
+++ internal/k8s/ingress.go
@@ -5,12 +5,13 @@ import (
 	"slices"
 
 	"github.com/airbytehq/abctl/internal/common"
+	"github.com/airbytehq/abctl/internal/helm"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Ingress creates an ingress type for defining the webapp ingress rules.
-func Ingress(hosts []string) *networkingv1.Ingress {
+func Ingress(chartVersion string, hosts []string) *networkingv1.Ingress {
 	var ingressClassName = "nginx"
 
 	// if no host is defined, default to an empty host
@@ -31,7 +32,7 @@ func Ingress(hosts []string) *networkingv1.Ingress {
 
 	var rules []networkingv1.IngressRule
 	for _, host := range hosts {
-		rules = append(rules, ingressRule(host))
+		rules = append(rules, ingressRules(chartVersion, host))
 	}
 
 	return &networkingv1.Ingress{
@@ -48,37 +49,83 @@ func Ingress(hosts []string) *networkingv1.Ingress {
 }
 
 // ingressRule creates a rule for the host with proper API routing.
-func ingressRule(host string) networkingv1.IngressRule {
-	var pathType = networkingv1.PathType("Prefix")
+func ingressRules(chartVersion string, host string) networkingv1.IngressRule {
+	rules := ingressRulesForV1()
+	if helm.ChartIsV2Plus(chartVersion) {
+		rules = ingressRulesForV2()
+	}
 
 	return networkingv1.IngressRule{
-		Host: host,
-		IngressRuleValue: networkingv1.IngressRuleValue{
-			HTTP: &networkingv1.HTTPIngressRuleValue{
-				Paths: []networkingv1.HTTPIngressPath{
-					// Route connector builder API to connector-builder-server
-					{
-						Path:     "/api/v1/connector_builder",
-						PathType: &pathType,
-						Backend: networkingv1.IngressBackend{
-							Service: &networkingv1.IngressServiceBackend{
-								Name: fmt.Sprintf("%s-airbyte-connector-builder-server-svc", common.AirbyteChartRelease),
-								Port: networkingv1.ServiceBackendPort{
-									Name: "http",
-								},
+		Host:             host,
+		IngressRuleValue: rules,
+	}
+}
+
+func ingressRulesForV1() networkingv1.IngressRuleValue {
+	var pathType = networkingv1.PathType("Prefix")
+
+	return networkingv1.IngressRuleValue{
+		HTTP: &networkingv1.HTTPIngressRuleValue{
+			Paths: []networkingv1.HTTPIngressPath{
+				// Route connector builder API to connector-builder-server
+				{
+					Path:     "/api/v1/connector_builder",
+					PathType: &pathType,
+					Backend: networkingv1.IngressBackend{
+						Service: &networkingv1.IngressServiceBackend{
+							Name: fmt.Sprintf("%s-airbyte-connector-builder-server-svc", common.AirbyteChartRelease),
+							Port: networkingv1.ServiceBackendPort{
+								Name: "http",
 							},
 						},
 					},
-					// Default route for everything else to webapp
-					{
-						Path:     "/",
-						PathType: &pathType,
-						Backend: networkingv1.IngressBackend{
-							Service: &networkingv1.IngressServiceBackend{
-								Name: fmt.Sprintf("%s-airbyte-webapp-svc", common.AirbyteChartRelease),
-								Port: networkingv1.ServiceBackendPort{
-									Name: "http",
-								},
+				},
+				// Default route for everything else to webapp
+				{
+					Path:     "/",
+					PathType: &pathType,
+					Backend: networkingv1.IngressBackend{
+						Service: &networkingv1.IngressServiceBackend{
+							Name: fmt.Sprintf("%s-airbyte-webapp-svc", common.AirbyteChartRelease),
+							Port: networkingv1.ServiceBackendPort{
+								Name: "http",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func ingressRulesForV2() networkingv1.IngressRuleValue {
+	var pathType = networkingv1.PathType("Prefix")
+
+	return networkingv1.IngressRuleValue{
+		HTTP: &networkingv1.HTTPIngressRuleValue{
+			Paths: []networkingv1.HTTPIngressPath{
+				// Route connector builder API to connector-builder-server
+				{
+					Path:     "/api/v1/connector_builder",
+					PathType: &pathType,
+					Backend: networkingv1.IngressBackend{
+						Service: &networkingv1.IngressServiceBackend{
+							Name: fmt.Sprintf("%s-airbyte-connector-builder-server-svc", common.AirbyteChartRelease),
+							Port: networkingv1.ServiceBackendPort{
+								Name: "http",
+							},
+						},
+					},
+				},
+				// Default route for everything else to the server
+				{
+					Path:     "/",
+					PathType: &pathType,
+					Backend: networkingv1.IngressBackend{
+						Service: &networkingv1.IngressServiceBackend{
+							Name: fmt.Sprintf("%s-airbyte-server-svc", common.AirbyteChartRelease),
+							Port: networkingv1.ServiceBackendPort{
+								Name: "http",
 							},
 						},
 					},
