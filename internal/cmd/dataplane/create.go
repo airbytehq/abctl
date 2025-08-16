dbe36ef33b9fa96d8d4c0872e2be287b4a5db02f
Bryce Groff <bgroff@hawaii.edu>
2025-08-16 15:31:01 -0700

    add the oidc url

--- internal/cmd/dataplane/create.go
+++ internal/cmd/dataplane/create.go
@@ -1,6 +1,9 @@
 package dataplane
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/pterm/pterm"
 )
 
@@ -8,6 +11,33 @@ type CreateCmd struct {
 }
 
 func (c *CreateCmd) Run() error {
-	pterm.Success.Println("Dataplane create command executed successfully")
+	// Prompt for base URL with default value
+	defaultURL := "https://cloud.airbyte.com"
+
+	urlPrompt := fmt.Sprintf("Enter the base URL of the Airbyte instance [%s]", defaultURL)
+	baseURL, _ := pterm.DefaultInteractiveTextInput.Show(urlPrompt)
+
+	// Use default if user didn't enter anything
+	baseURL = strings.TrimSpace(baseURL)
+	if baseURL == "" {
+		baseURL = defaultURL
+	}
+
+	pterm.Info.Printf("Using Airbyte instance at: %s\n", baseURL)
+
+	// Prompt for OIDC server root with default value
+	defaultOIDCServer := "https://cloud.airbyte.com/auth"
+	
+	oidcPrompt := fmt.Sprintf("Enter the OIDC server root [%s]", defaultOIDCServer)
+	oidcServer, _ := pterm.DefaultInteractiveTextInput.Show(oidcPrompt)
+	
+	// Use default if user didn't enter anything
+	oidcServer = strings.TrimSpace(oidcServer)
+	if oidcServer == "" {
+		oidcServer = defaultOIDCServer
+	}
+	
+	pterm.Info.Printf("Using OIDC server at: %s\n", oidcServer)
+
 	return nil
 }
